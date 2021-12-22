72b32a922216aa1132d01a1db985aa4c29b2f5d4
Django9264 <82198758+Django9264@users.noreply.github.com>
2021-12-22 21:37:49 +0800

    update

--- files/luci/controller/tencentddns.lua
+++ files/luci/controller/tencentddns.lua
@@ -1,5 +1,4 @@
 module("luci.controller.tencentddns",package.seeall)
 function index()
-entry({"admin", "tencentcloud"}, firstchild(), "腾讯云设置", 30).dependent=false
-entry({"admin", "tencentcloud", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),2)
+entry({"admin", "services", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"), 12)
 end
